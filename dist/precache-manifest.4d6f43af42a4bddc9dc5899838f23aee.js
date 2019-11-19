5f8853cd06e5f04a31d52c504e0c3b6830059db2
Pierrick <pierrickblot243@gmail.com>
2019-11-19 13:20:39 +0100

    deuxieme commit

---
+++ dist/precache-manifest.4d6f43af42a4bddc9dc5899838f23aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93390a31417b386db58",
+    "url": "/css/chunk-vendors.69c1ee87.css"
+  },
+  {
+    "revision": "f2cddff00d523ffb2e16321b762274d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3024ce5e1fb9db2e7ced",
+    "url": "/js/app.7bbfd0f9.js"
+  },
+  {
+    "revision": "a93390a31417b386db58",
+    "url": "/js/chunk-vendors.d277a538.js"
+  },
+  {
+    "revision": "c86c558eebbe07494d563a2cb9c8561c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
